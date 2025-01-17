9bafbf66a573abe8a52afa5820509f440d14023e
Ioannis Sarridis <gsarridis@gmail.com>
2025-01-17 16:47:05 +0200

    show log in readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,4 +23,5 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
+html_logo = "_static/vb-mitigator logo_250.png"
 html_static_path = ["_static"]
